480f5046ce3c9d07c55ff58daa2b513cfdd3777d
Ivan Joshua Punzalan Arsua <iparsua@csmbataan.ph.education>
2020-12-14 21:25:27 -0800

    Added a public class named News

--- Ivan.Arsua Second Quarterly Assesment/Ivan.Arsua Second Quarterly Assesment/MainWindow.xaml.cs
+++ Ivan.Arsua Second Quarterly Assesment/Ivan.Arsua Second Quarterly Assesment/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace Ivan.Arsua_Second_Quarterly_Assesment
 {
@@ -23,11 +26,22 @@ namespace Ivan.Arsua_Second_Quarterly_Assesment
         public MainWindow()
         {
             InitializeComponent();
+            var url = "http://newsapi.org/v2/top-headlines?" +
+            "country=ph&" +
+            "category=business" +
+            "apiKey=21abf07344d6405e8b0c8a04d2de23b8";
+
+            var json = new WebClient().DownloadString(url);
+
+            Console.WriteLine(json);
         }
 
+        
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
         }
+        
+           
     }
 }
